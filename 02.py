68b53271821141bd1878fb77ba234738bedf2c20
gastonbar79-gif <gastonbar79@gmail.com>
2026-03-05 12:50:14 -0300

    Update 02.py

--- 02.py
+++ 02.py
@@ -1,1 +1,10 @@
+temperatura = 25
 
+if temperatura > 30:
+    print("Hace mucho calor, mejor usa shorts.")
+elif temperatura >= 20:
+    print("El clima está agradable, una remera está bien.")
+elif temperatura >= 10:
+    print("Está fresco, mejor llevate un buzo.")
+else:
+    print("Hace frío, ponete una campera abrigada.")
